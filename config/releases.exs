2e8bdbede36e220bf03c7784b4323e737c4f446b
Brian Petersen <skiwalk3r@gmail.com>
2020-08-20 18:51:49 -0600

    Runs mix format

--- config/releases.exs
+++ config/releases.exs
@@ -1,4 +1,3 @@
 import Config
 
-config :picoquotes, PicoquotesWeb.Endpoint,
-  secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
+config :picoquotes, PicoquotesWeb.Endpoint, secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
