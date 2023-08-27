4c7c9480a2b732b0bcea0ccc4fa442cbfbe80c44
Brian Petersen <skiwalk3r@gmail.com>
2023-08-26 19:45:24 -0600

    Addresses updated dep warnings

--- lib/picoquotes_web/views/base_view.ex
+++ lib/picoquotes_web/views/base_view.ex
@@ -9,10 +9,8 @@ defmodule PicoquotesWeb.BaseView do
         root: "lib/picoquotes_web/templates",
         namespace: PicoquotesWeb
 
+      alias Phoenix.Flash
       alias PicoquotesWeb.Router.Helpers, as: Routes
-
-      # Import convenience functions from controllers
-      import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
 
       import PicoquotesWeb.Authentication, only: [signed_in?: 1]
       import PicoquotesWeb.ErrorHelpers
