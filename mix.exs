d9cd54eeb27b950c89d586c6f28b742c89fa2584
Brian Petersen <skiwalk3r@gmail.com>
2026-08-22 08:02:24 -0600

    Updates deps

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule MyPersonalSpace.MixProject do
       {:phoenix_html, "~> 4.0"},
       {:phoenix_html_helpers, "~> 1.0"},
       {:phoenix_html_sanitizer, "~> 1.0"},
-      {:phoenix_live_dashboard, "~> 0.8.0"},
+      {:phoenix_live_dashboard, "~> 0.9.0"},
       {:phoenix_live_reload, "~> 1.3", only: :dev},
       {:phoenix_live_view, "~> 1.0"},
       {:phoenix_pubsub, "~> 2.0"},
